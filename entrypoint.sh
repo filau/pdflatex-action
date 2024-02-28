36d6ba572bb4f3cca197ab8f36cafde45a972c34
Kristian Evers <kristian@evers.dev>
2024-02-28 14:52:07 +0100

    Run pdflatex twice
    Often needed when  there's cross-references in the document

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,4 @@ echo "Running pdflatex on: $1"
 echo "Current directory: $(pwd)"
 echo "Output directory: $GITHUB_WORKSPACE"
 pdflatex -output-directory=$GITHUB_WORKSPACE $1
+pdflatex -output-directory=$GITHUB_WORKSPACE $1
